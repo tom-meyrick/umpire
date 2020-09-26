db236d81a16740de6c77cbbf98adaa3c396e2031
Tom Meyrick <tomgmeyrick@gmail.com>
2020-09-26 07:33:48 +0100

    Updates

---
+++ precache-manifest.5c55ded46318602b6309503c16b62ff5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dbc4705686fb16abafaa74f0e3e5eeb",
+    "url": "/umpire/index.html"
+  },
+  {
+    "revision": "b39fefbfaed59214d3b8",
+    "url": "/umpire/static/css/main.819ad5d7.chunk.css"
+  },
+  {
+    "revision": "7c2cfceb3f768ed9b102",
+    "url": "/umpire/static/js/2.4b38e4b4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/umpire/static/js/2.4b38e4b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b39fefbfaed59214d3b8",
+    "url": "/umpire/static/js/main.ed1dad22.chunk.js"
+  },
+  {
+    "revision": "a9b822d88b3ac0fb6a53",
+    "url": "/umpire/static/js/runtime-main.1550375a.js"
+  }
+]);
